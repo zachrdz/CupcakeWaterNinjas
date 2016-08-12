9e3cc14b4fd3558191f7a32aca1d835937aac521
zachrdz <zjrodr0709@hotmail.com>
2016-08-12 17:58:19 -0500

    Added styling to individual recipe page and some formatting on the fields.

--- app/views/recipes/viewrecipe.blade.php
+++ app/views/recipes/viewrecipe.blade.php
@@ -2,86 +2,112 @@
 
 @section('head')
 <link rel='icon' type='image/png' href='http://i.imgur.com/jm7EdAX.png' />
+
+<link rel="stylesheet" href="../js/dropzone/dist/min/dropzone.min.css">
+<link rel="stylesheet" href="../css/view_recipe.css">
+
+<!-- Include js plugin -->
+<script src="../js/dropzone/dist/min/dropzone.min.js"></script>
 @endsection
 
 @section('content')
-<p>
-  {{$recipe->recipe_name}}
-</p>
-<p>
-  {{$recipe->difficulty}}
-</p>
-<p>
-  {{$recipe->likes}}
-</p>
-<p>
-  {{$recipe->dislikes}}
-</p>
-<p>
-  {{$recipe->views}}
-</p>
-<!--make sure to have a delmin split by ' '-->
-<p>
-  {{$recipe->ingredients}}
-</p>
-<!--make sure to have a delmin split by ' '-->
-<p>
-  {{$recipe->directions}}
-</p>
-<p>
-  {{$recipe->recipe_pic}}
-</p>
-<p>
-  {{$recipe->created_at}}
-</p>
+<!--
+    Available Recipe Fields:
+    name
+    difficulty
+    likes
+    dislikes
+    views
+    directions
+    ingredients
+    recipe_pic
+    created_at
+-->
 
-
-
-
+<!--recipe section -->
+<div class="container">
+	<div class="row">
+        <div class="col-md-4">
+            <h3>Name</h3>
+            <p>{{$recipe->recipe_name}}</p>
+            <br><br><hr>
+            <h5>Difficulty</h5>
+            <span>{{$recipe->difficulty}}</span>
+        </div>
+        <div class="col-md-8 recipe_main_image">
+            <img src="{{$recipe->recipe_pic}}" height="300px">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Ingredients</h3>
+            <p>{{$recipe->ingredients}}</p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Directions</h3>
+            <p>{{$recipe->directions}}</p>
+        </div>
+    </div>
+    <br>
+</div>
 
 <!--comments section -->
-<div class="row">
-  <div class="col-md-6 myform">
+<div class="container">
+    <hr>
+    <div class="row">
+        <div>
+            @if(Session::has('error_messages'))
+            <div class="alert alert-danger" role="alert">
+                {{Session::get('error_messages')}}
+            </div>
+            @endif
 
-    @if(Session::has('error_messages'))
-
-    <div class="alert alert-danger" role="alert">
-      {{Session::get('error_messages')}}
+            @if(Auth::check())
+            <div class="well" style="padding-bottom: 0px;">
+                {{Form::open(['id' => 'commentId','action' => 'RecipeController@createComment','role' => 'form', 'method'=> 'POST'])}}
+                <div class="form-group">
+                    <label for="comment">Comment</label>
+                    <textarea name="comment" style="height: 70px;"class="form-control"></textarea>
+                </div>
+                {{Form::hidden('id', $recipe->id)}}
+                <div class="form-group">
+                    {{Form::submit('Submit',['class' => 'btn btn-primary'])}}
+                </div>
+                {{Form::close()}}
+            </div>
+            @endif
+        </div>
     </div>
 
+    <!-- show all the comments -->
+    @foreach($comments as $comment)
+    <?php
+        $date = new DateTime($comment->created_at);
+        $created_at = $date->format('m\/d\/y \a\t h:i:sa');
+    ?>
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="thumbnail">
+                <img class="img-responsive user-photo" src='{{$comment->profile_pic}}'>
+            </div><!-- /thumbnail -->
+        </div><!-- /col-sm-1 -->
 
-    @endif
-  </div>
+        <div class="col-sm-10">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong>{{$comment->name}}</strong> <span class="text-muted">{{$created_at}}</span>
+                </div>
+                <div class="panel-body">
+                    {{$comment->comment}}
+                </div><!-- /panel-body -->
+            </div><!-- /panel panel-default -->
+        </div><!-- /col-sm-5 -->
+    </div><!-- /row -->
+    @endforeach
+
 </div>
-@if(Auth::check())
-{{Form::open(['id' => 'commentId','action' => 'RecipeController@createComment', 'class' => 'form', 'method'=> 'POST'])}}
-  <div class="form-group">
-    <textarea name="comment"></textarea>
-  </div>
-  {{Form::hidden('id', $recipe->id)}}
-  <div class="form-group">
-    {{Form::submit('Comment',['class' => 'btn btn-primary'])}}
-  </div>
-{{Form::close()}}
-@endif
-
-<!-- show all the comments -->
-@foreach($comments as $comment)
-<p>
-  {{$comment->user_id}}
-</p>
-<p>
-  {{$comment->recipe_id}}
-</p>
-<p>
-  {{$comment->comment}}
-</p>
-<p>
-  {{$comment->fname}}
-</p>
-<img src='{{$comment->profile_pic}}' alt="Smiley face" height="42" width="42">
-<p>
-  {{$comment->created_at}}
-</p>
-@endforeach
 @endsection
